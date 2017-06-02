0b6fad7091d3b2a446233bb1663ae2b82fe68350
Adilson Tavares <adilsonmktavares@gmail.com>
2017-06-01 23:51:01 -0300

    Adicionado scroll to top

--- js/myscript.js
+++ js/myscript.js
@@ -1,7 +1,10 @@
+$(function() {
+	$('body').materialScrollTop();
+});
 
-  $(document).ready(function(){
-      $('.parallax').parallax();
-    });
+$(document).ready(function(){
+  $('.parallax').parallax();
+});
 // Initialize collapse button
   $(".button-collapse").sideNav();
   // Initialize collapsible (uncomment the line below if you use the dropdown variation)
